6c5539a616990141e472f21cc37a58db81a90b2d
Nykarri <karri.bonez@gmail.com>
2020-09-30 17:26:33 -0400

    confi routes for api gateway

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,22 @@ Rails.application.routes.draw do
     end                                                                          
   end     
   resources :posts
+
+  #API GATEWAY URLS
+  #users
+  get '/admin/users', to: 'users#index'
+  post '/admin/users/login', to: 'users#login'
+  post '/admin/users', to: 'users#create'
+  get '/admin/users/:id', to: 'users#show'
+  put '/admin/users/:id', to: 'users#update'
+  delete '/admin/users/:id', to: 'users#destroy'
+
+  #posts
+  get '/admin/posts', to: 'posts#index'
+  post '/admin/posts', to: 'posts#create'
+  get '/admin/posts/:id', to: 'posts#show'
+  put '/admin/posts/:id', to: 'posts#update'
+  delete '/admin/posts/:id', to: 'posts#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
